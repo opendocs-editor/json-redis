7806e6726b782776e16a860a2166ab24dec286f8
Jacob Sapoznikow <jacob1coder@gmail.com>
2022-01-16 03:06:26 +0000

    Add support for other hosts and authentication

--- src/callback.ts
+++ src/callback.ts
@@ -5,7 +5,7 @@ interface EventHandlers {
 }
 
 interface FlexableObject extends Object {
-    [key: string]: unknown
+    [key: string]: unknown;
 }
 
 export default class RedisJSONCallbackClient {
@@ -14,15 +14,43 @@ export default class RedisJSONCallbackClient {
         redis.RedisScripts
     >;
     private events: EventHandlers = {};
-    private hasInitialized: boolean = false;
+    private hasInitialized = false;
 
     constructor(
-        _client?: redis.RedisClientType<redis.RedisModules, redis.RedisScripts>
+        _client?: redis.RedisClientType<redis.RedisModules, redis.RedisScripts>,
+        host?: string,
+        port?: number,
+        user?: string,
+        pass?: string,
+        secure?: boolean,
+        dbNumber?: number
     ) {
         if (_client) {
             this.client = _client;
         } else {
-            this.client = redis.createClient();
+            if(host && port && user && pass && secure && dbNumber) {
+                this.client = redis.createClient({
+                    url: `redis${secure === true ? "s" : ""}://${user}:${pass}@${host}:${port}/${dbNumber}`
+                });
+            } else if(host && port && user && pass && secure) {
+                this.client = redis.createClient({
+                    url: `redis${secure === true ? "s" : ""}://${user}:${pass}@${host}:${port}`
+                });
+            } else if(host && port && user && pass) {
+                this.client = redis.createClient({
+                    url: `redis://${user}:${pass}@${host}:${port}`
+                });
+            } else if(host && port) {
+                this.client = redis.createClient({
+                    url: `redis://${host}:${port}`
+                });
+            } else if(host) {
+                this.client = redis.createClient({
+                    url: `redis://${host}`
+                });
+            } else {
+                this.client = redis.createClient();
+            }
         }
         this.client.on("error", (err) => {
             console.log(
@@ -39,7 +67,7 @@ export default class RedisJSONCallbackClient {
             for (let i = 0; i < this.events.init.length; i++) {
                 this.events.init[i]();
             }
-        } catch(e) {
+        } catch (e) {
             console.error(e);
             return false;
         }
@@ -50,23 +78,42 @@ export default class RedisJSONCallbackClient {
         this.events[event].push(handler);
     }
 
-    public set(key: string, value: FlexableObject, callback: (data: string | null) => void): void {
-        if(!this.hasInitialized) throw new ReferenceError("The client has not initialized yet!");
-        this.client.set(key, JSON.stringify(value)).then((value) => {
-            callback(value);
-        }).catch((err) => {
-            console.log(`\x1b[41m\x1b[30m ERROR \x1b[0m\x1b[36mRedis Error:\x1b[0m\n${err}`);
-            callback(null);
-        });
+    public set(
+        key: string,
+        value: FlexableObject,
+        callback: (data: string | null) => void
+    ): void {
+        if (!this.hasInitialized)
+            throw new ReferenceError("The client has not initialized yet!");
+        this.client
+            .set(key, JSON.stringify(value))
+            .then((value) => {
+                callback(value);
+            })
+            .catch((err) => {
+                console.log(
+                    `\x1b[41m\x1b[30m ERROR \x1b[0m\x1b[36mRedis Error:\x1b[0m\n${err}`
+                );
+                callback(null);
+            });
     }
 
-    public get(key: string, callback: (data: FlexableObject | null) => void): void {
-        if(!this.hasInitialized) throw new ReferenceError("The client has not initialized yet!");
-        this.client.get(key).then((value) => {
-            callback(JSON.parse(value as string) as FlexableObject);
-        }).catch((err) => {
-            console.log(`\x1b[41m\x1b[30m ERROR \x1b[0m\x1b[36mRedis Error:\x1b[0m\n${err}`);
-            callback(null);
-        });
+    public get(
+        key: string,
+        callback: (data: FlexableObject | null) => void
+    ): void {
+        if (!this.hasInitialized)
+            throw new ReferenceError("The client has not initialized yet!");
+        this.client
+            .get(key)
+            .then((value) => {
+                callback(JSON.parse(value as string) as FlexableObject);
+            })
+            .catch((err) => {
+                console.log(
+                    `\x1b[41m\x1b[30m ERROR \x1b[0m\x1b[36mRedis Error:\x1b[0m\n${err}`
+                );
+                callback(null);
+            });
     }
 }
--- src/promise.ts
+++ src/promise.ts
@@ -5,7 +5,7 @@ interface EventHandlers {
 }
 
 interface FlexableObject extends Object {
-    [key: string]: unknown
+    [key: string]: unknown;
 }
 
 export default class RedisJSONPromiseClient {
@@ -14,15 +14,43 @@ export default class RedisJSONPromiseClient {
         redis.RedisScripts
     >;
     private events: EventHandlers = {};
-    private hasInitialized: boolean = false;
+    private hasInitialized = false;
 
     constructor(
-        _client?: redis.RedisClientType<redis.RedisModules, redis.RedisScripts>
+        _client?: redis.RedisClientType<redis.RedisModules, redis.RedisScripts>,
+        host?: string,
+        port?: number,
+        user?: string,
+        pass?: string,
+        secure?: boolean,
+        dbNumber?: number
     ) {
         if (_client) {
             this.client = _client;
         } else {
-            this.client = redis.createClient();
+            if(host && port && user && pass && secure && dbNumber) {
+                this.client = redis.createClient({
+                    url: `redis${secure === true ? "s" : ""}://${user}:${pass}@${host}:${port}/${dbNumber}`
+                });
+            } else if(host && port && user && pass && secure) {
+                this.client = redis.createClient({
+                    url: `redis${secure === true ? "s" : ""}://${user}:${pass}@${host}:${port}`
+                });
+            } else if(host && port && user && pass) {
+                this.client = redis.createClient({
+                    url: `redis://${user}:${pass}@${host}:${port}`
+                });
+            } else if(host && port) {
+                this.client = redis.createClient({
+                    url: `redis://${host}:${port}`
+                });
+            } else if(host) {
+                this.client = redis.createClient({
+                    url: `redis://${host}`
+                });
+            } else {
+                this.client = redis.createClient();
+            }
         }
         this.client.on("error", (err) => {
             console.log(
@@ -39,7 +67,7 @@ export default class RedisJSONPromiseClient {
             for (let i = 0; i < this.events.init.length; i++) {
                 this.events.init[i]();
             }
-        } catch(e) {
+        } catch (e) {
             console.error(e);
             return false;
         }
@@ -50,22 +78,33 @@ export default class RedisJSONPromiseClient {
         this.events[event].push(handler);
     }
 
-    public async set(key: string, value: FlexableObject): Promise<string | null> {
+    public async set(
+        key: string,
+        value: FlexableObject
+    ): Promise<string | null> {
         try {
-            if(!this.hasInitialized) throw new ReferenceError("The client has not initialized yet!");
+            if (!this.hasInitialized)
+                throw new ReferenceError("The client has not initialized yet!");
             return await this.client.set(key, JSON.stringify(value));
-        } catch(err) {
-            console.log(`\x1b[41m\x1b[30m ERROR \x1b[0m\x1b[36mRedis Error:\x1b[0m\n${err}`);
+        } catch (err) {
+            console.log(
+                `\x1b[41m\x1b[30m ERROR \x1b[0m\x1b[36mRedis Error:\x1b[0m\n${err}`
+            );
             return null;
         }
     }
 
     public async get(key: string): Promise<FlexableObject | null> {
         try {
-            if(!this.hasInitialized) throw new ReferenceError("The client has not initialized yet!");
-            return (JSON.parse((await this.client.get(key)) as string)) as FlexableObject;
-        } catch(err) {
-            console.log(`\x1b[41m\x1b[30m ERROR \x1b[0m\x1b[36mRedis Error:\x1b[0m\n${err}`);
+            if (!this.hasInitialized)
+                throw new ReferenceError("The client has not initialized yet!");
+            return JSON.parse(
+                (await this.client.get(key)) as string
+            ) as FlexableObject;
+        } catch (err) {
+            console.log(
+                `\x1b[41m\x1b[30m ERROR \x1b[0m\x1b[36mRedis Error:\x1b[0m\n${err}`
+            );
             return null;
         }
     }
